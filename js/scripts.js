88069bdc322815387b862214a5d18ba1de437f33
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2020-10-20 11:38:44 -0700

    play peek-a-boo

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
-});
 
+  $("#fade_toggle").click(function() {
+    $("p").fadeToggle();
+    $("p").fadeToggle();
+  });
+  $(".click_here").click(function() {
+    $(".slideToggle").slideToggle();
+    $(".slideToggle").slideToggle();
+  });
+});
